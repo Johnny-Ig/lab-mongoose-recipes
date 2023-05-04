36bd8b7f25d616cf5e0e40c7152f1cafae86a108
Jonatan Iglesias <jonathan-18719@outlook.es>
2023-05-04 20:53:39 +0200

    Completed lab

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const Recipe = require('./models/Recipe.model');
 // Import of the data from './data.json'
 const data = require('./data');
 
-const MONGODB_URI = 'mongodb://localhost:27017/recipe-app';
+const MONGODB_URI = 'mongodb://127.0.0.1:27017/recipe-app';
 
 // Connection to the database "recipe-app"
 mongoose
@@ -17,7 +17,39 @@ mongoose
   })
   .then(() => {
     // Run your code here, after you have insured that the connection was made
+    return Recipe.create({
+      title: "Asado",
+      level: "Amateur Chef",
+      ingredients: [
+        "Costilla",
+        "Chorizo",
+        "Entraña",
+        "Chinchulines",
+      ],
+      cuisine: "Argentina",
+      dishType: "main_course",
+      image: "https://images.media-allrecipes.com/userphotos/720x405/815964.jpg",
+      duration: 40,
+      creator: "Johnny"
+    })
+    
   })
+  .then(()=>{
+    return Recipe.insertMany(data)
+  })
+
+  .then(()=>{
+    return Recipe.findOneAndUpdate(
+     
+      {title:"Rigatoni alla Genovese"},{
+        duration:
+        100} )
+  })
+  
+ 
+
   .catch(error => {
     console.error('Error connecting to the database', error);
+    
+    
   });
